b6ec9fa9dfef69e055e986a96f2710fa280f929b
Pawel <pawix@gmail.com>
2021-04-25 10:12:14 +0200

    add app structure and delete function

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class AddTask extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<div>
+				<div>Dodaj task</div>
+				<hr />
+			</div>
+		);
+	}
+}
+
+export default AddTask;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,89 @@
 import React, { Component } from "react";
 import "./App.css";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			tasks: [
+				{
+					id: 0,
+					text: "zagrać w gre",
+					date: "2021-05-20",
+					importat: true,
+					active: true,
+					finishDate: null
+				},
+				{
+					id: 1,
+					text: "posprzatac",
+					date: "2022-09-10",
+					importat: false,
+					active: true,
+					finishDate: null
+				},
+				{
+					id: 3,
+					text: "zdac studia",
+					date: "2023-04-23",
+					importat: true,
+					active: true,
+					finishDate: null
+				},
+				{
+					id: 4,
+					text: "task1",
+					date: "2023-04-23",
+					importat: true,
+					active: true,
+					finishDate: null
+				},
+				{
+					id: 5,
+					text: "task5",
+					date: "2023-04-23",
+					importat: true,
+					active: true,
+					finishDate: null
+				}
+			]
+		};
 	}
+
+	// Usuwanie obiektow z tablicy ze state
+	deleteTask = id => {
+		//console.log("delete id " + id);
+		const tasks = [...this.state.tasks];
+		const index = tasks.findIndex(task => task.id === id);
+		//console.log(tasks);
+		if (index === -1) {
+			throw Error("Index not found");
+		}
+		tasks.splice(index, 1);
+		//console.log(index);
+		this.setState({
+			tasks: tasks
+		});
+	};
+
+	changeTaskStatus = id => {
+		console.log("change id " + id);
+	};
+
 	render() {
-		return <div className="App">To do</div>;
+		return (
+			<div className="App">
+				To do
+				<AddTask />
+				<TaskList
+					tasks={this.state.tasks}
+					delete={this.deleteTask}
+					change={this.changeTaskStatus}
+				/>
+			</div>
+		);
 	}
 }
 
